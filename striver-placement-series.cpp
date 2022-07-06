8a25cf40082caea259117a930413fe970f17ebba
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-07-06 21:04:48 +0530

    placement series.

--- striver-placement-series.cpp
+++ striver-placement-series.cpp
@@ -9,7 +9,12 @@ class placement{
         int cnt=0;
         int j=mid+1;
         for(int i=low; i<=mid; i++){
-            while((j<=high) && ((long)nums[i] > (long)2*nums[j])){
+            /* Below (2 * nums[j]) can occure in "integer overflow"
+               so either type cast them to long to avoid that  
+               ((long)nums[i] > (long)2*nums[j]))
+               or use 2L so that it can auto type cast to long.
+               */
+            while((j<=high) && (nums[i] > 2L *nums[j])){
                 j++;
             }
             cnt += (j - (mid+1));
@@ -45,7 +50,16 @@ class placement{
         return inv;
     }
 
+    int grid_recurssive_paths(int i, int j, int m, int n){
+        if(i==m-1 && j==n-1)
+            return 1;
+        if(i>=m || j>=n)
+            return 0;
+        else
+            return (grid_recurssive_paths(i+1,j,m,n)+grid_recurssive_paths(i,j+1,m,n));
+    }
     public:
+
     vector<int> twoSum(vector<int>&arr, int sum){
         /* Return the indices of the two numbers such that they add upto the target.
            Don't use same element twice, exactly one solution */
@@ -68,20 +82,36 @@ class placement{
     int find_inversion(vector<int>& nums){
         return merge_sort(nums,0,nums.size()-1);
     }
+
+    /* The robot is placed in top left corner, it can move either right or down.
+       The robot is trying to reach the bottom right corner of the grid.
+       How many possible unique paths are there.
+    */
+    int unique_paths(int m, int n){
+        /* recurssive solution */
+        return grid_recurssive_paths(0,0,m,n);
+        /* DP */
+    }
 };
 
 int main(){
     placement ob;
     
-    //Two SUM
+    //1. Two SUM
     vector<int> arr({2,3,5,7,9,4,6});
     int sum=8;
     vector<int> res = ob.twoSum(arr,sum);
     cout<<"Two sum for "<<sum<<" is "<<arr[res[0]]<<" "<<arr[res[1]]<<endl;
 
-    //find inversion
+    //2. find inversion
     vector<int> arr1({2,4,3,5,1});
     cout<<"Number of inversions "<<ob.find_inversion(arr1)<<endl;
+
+    //3. grid Unique path
+    //https://leetcode.com/problems/unique-paths/discuss/22954/C%2B%2B-DP
+    cout<<"Number of Unique paths "<<ob.unique_paths(3,7)<<endl;
+
+
 
     return 0;
 }
