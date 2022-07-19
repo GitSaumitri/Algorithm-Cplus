5e124727685e86d1d9eef1d206bc3e7edeeb5096
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-07-19 08:58:02 +0530

    recursion series - combination sum - 19/07/2022.

--- striver-recursion-series.cpp
+++ striver-recursion-series.cpp
@@ -16,9 +16,9 @@
       - maintain order of elements / otherwise it's a subarray
       - alternative way is through powerset
     7. Printing subsequences whose sum is K
-        - 
-*/
+    8. Combination SUM
 
+*/
 
 #include<iostream>
 using namespace std;
@@ -36,7 +36,12 @@ class recursionSeries{
     bool printSubsequencesSUM1(int ind, vector<int>& ds, int arr[], int n, int sum, int s);
         // only return count
     int printSubsequencesSUM2(int ind, vector<int>& ds, int arr[], int n, int sum, int s);
-    
+
+    //8. Combination SUM
+    void findCombinations(int ind, int target, vector<int>& arr, vector<vector<int>>& ans, vector<int>& ds);
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target);
+
+    //9.
 };
 
 int main(){
@@ -56,6 +61,20 @@ int main(){
     cout<<endl<<"Print Subsequences of SUM "<<sum<<" first "<<endl;
     ob.printSubsequencesSUM1(0, ds7, arr7, n, sum, 0);
     cout<<endl<<"Count "<<ob.printSubsequencesSUM2(0, ds7, arr7, n, sum, 0)<<endl;
+
+    //8. Combination SUM
+    vector<int> arr8({2,3,6,7}); n = 4, sum=7; vector<int> ds8; vector<vector<int>> res8;
+    res8 = ob.combinationSum(arr8, sum);
+    cout<<"Combination SUM of "<<sum<<" "<<endl;
+    for(int i=0; i<res8.size(); i++){
+        cout<<"{ ";
+        for(int j=0; j<res8[i].size(); j++){
+            cout<<res8[i][j]<<" ";
+        }
+        cout<<"} ";
+    }
+    cout<<endl;
+
 
     return 0;
 }
@@ -191,4 +210,37 @@ int recursionSeries::printSubsequencesSUM2(int ind, vector<int>& ds, int arr[], 
 
     //if non of the cases hit
     return l+r;
+}
+
+/* 8. Combination SUM
+   Given an array of distinct integers and a target integer, return a list of all unique
+   combination of candidates where the chosen number sum to target. You may return the 
+   candidate in any order.
+   The same number may be chosen from candidates an unlimited number of times, two 
+   combinations are unique, if the frequency of at least one of the chosen number is
+   different.  
+        - O(2^t)*O(k)
+*/
+void recursionSeries::findCombinations(int ind, int target, vector<int>& arr, vector<vector<int>>& ans, vector<int>& ds){
+    if(ind == arr.size()){
+        if(target == 0){
+            ans.push_back(ds);
+        }
+        return;
+    }
+    //pick up the element
+    if(arr[ind] <= target){
+        ds.push_back(arr[ind]);
+        findCombinations(ind, target-arr[ind], arr, ans, ds);
+        ds.pop_back();
+    }
+
+    findCombinations(ind+1, target, arr, ans, ds);
+}
+
+vector<vector<int>> recursionSeries::combinationSum(vector<int>& candidates, int target){
+    vector<vector<int>> ans;
+    vector<int> ds;
+    findCombinations(0, target, candidates, ans, ds);
+    return ans;
 }
