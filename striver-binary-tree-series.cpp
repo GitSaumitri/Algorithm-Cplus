de25533b37b3673779b650d24d1aaf187ef5fd67
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-07-22 00:24:27 +0530

    binary tree series - bottom view traversal - 22/07/2022.

--- striver-binary-tree-series.cpp
+++ striver-binary-tree-series.cpp
@@ -150,6 +150,9 @@ class BinaryTreeSeries{
 
     /* 22. top view traversal */
     vector<int> topViewTraversal(TreeNode *root);
+
+    /* 23. Bottom view traversal */
+    vector<int> bottomViewTraversal(TreeNode *root);
 };
 
 /* 5. recurssive preorder traversal */
@@ -523,6 +526,33 @@ vector<int> BinaryTreeSeries::topViewTraversal(TreeNode *root){
     return ans;
 }
 
+/* 23. Bottom view traversal 
+*/
+vector<int> BinaryTreeSeries::bottomViewTraversal(TreeNode *root){
+    vector<int> ans;
+    if(root == nullptr) 
+        return ans;
+    map<int,int> mpp;
+    queue<pair<TreeNode *,int>> q;
+    q.push({root,0});
+    while(!q.empty()){
+        auto it = q.front();
+        q.pop();
+        TreeNode *node = it.first;
+        int line = it.second;
+        mpp[line] = node->data;
+        if(node->left)
+            q.push({node->left, line-1});
+        if(node->right)
+            q.push({node->right, line+1});
+    }
+    for(auto it: mpp){
+        ans.push_back(it.second);
+    }
+    return ans;
+}
+
+
 int main(){
 
     BinaryTreeSeries ob;
@@ -629,6 +659,15 @@ int main(){
     cout<<"Top view of the binary tree: ";
     for(int i=0; i<res22.size(); i++){
         cout<<res22[i]<<" ";
+    }
+    cout<<endl;
+
+    /* 23. Bottom view of a binary tree */
+    vector<int> res23;
+    res23 = ob.bottomViewTraversal(ob.root);
+    cout<<"Bottom view of the binary tree: ";
+    for(int i=0; i<res23.size(); i++){
+        cout<<res23[i]<<" ";
     }
     cout<<endl;
 
