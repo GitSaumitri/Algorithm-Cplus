bdce639f16aa0f8543e145b4fb05550d0f06cd4d
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-07-24 23:15:42 +0530

    recursion series - n queens - 24/07/2022.

--- striver-recursion-series.cpp
+++ striver-recursion-series.cpp
@@ -71,6 +71,11 @@ class recursionSeries{
     void recurPermute1(int index, vector<int> &nums, vector<vector<int>>& ans);
     vector<vector<int>> permute1(vector<int>& nums);
 
+    //14. N queens
+    bool isSafe(int row, int col, vector<string> board, int n);
+    void solve(int col, vector<string>& board, vector<vector<string>>& ans, int n);
+    vector<vector<string>> solveQueens(int n);
+
 };
 
 int main(){
@@ -164,7 +169,19 @@ int main(){
         cout<<"}";
     }
     cout<<endl;
- 
+
+    //14 N queens
+    vector<vector<string>> ans14;
+    n=4;
+    ans14 = ob.solveQueens(4);
+    cout<<"N Queens: "<<ans14.size()<<" "<<ans14[0].size()<<endl;
+    for(int i=0; i<ans14.size(); i++){
+        for(int k=0; k<ans14[0].size(); k++){
+            cout<<ans14[i][k]<<endl;
+        }
+        cout<<endl;
+    }
+    cout<<endl;
  
 
     return 0;
@@ -460,3 +477,61 @@ vector<vector<int>> recursionSeries::permute1(vector<int>& nums){
     return ans;
 }
 
+/*  N-Queens
+    
+*/
+bool recursionSeries::isSafe(int row, int col, vector<string> board, int n){
+    //check upper diagonal
+    int drow = row;
+    int dcol = col;
+
+    while(row>=0 && col>=0){
+        if(board[row][col]=='Q') return false;
+        row--;
+        col--;
+    }
+
+    col = dcol;
+    row = drow;
+    while(col>=0){
+        if(board[row][col]=='Q') return false;
+        col--;
+    }
+
+    row = drow;
+    col = dcol;
+    while(row<n && col>=0){
+        if(board[row][col]=='Q') return false;
+        row++;
+        col--;
+    }
+
+    return true;
+}
+
+void recursionSeries::solve(int col, vector<string>& board, vector<vector<string>>& ans, int n){
+    if(col == n){
+        //cout<<"put board in"<<endl;
+        ans.push_back(board);
+        return;
+    }
+
+    for(int row=0; row<n; row++){
+        if(isSafe(row, col, board, n)){
+            board[row][col]='Q';
+            solve(col+1, board, ans, n);
+            board[row][col]='.';
+        }
+    }
+}
+
+vector<vector<string>> recursionSeries::solveQueens(int n){
+    vector<vector<string>> ans;
+    vector<string> board(n);
+    string s(n, '.');
+    for(int i=0; i<n; i++){
+        board[i]=s;
+    }
+    solve(0, board, ans, n);
+    return ans;
+}
