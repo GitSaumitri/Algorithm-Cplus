58d62a0e408f90aee2ee42b3852e7393e55e4688
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-09-13 08:20:16 +0530

    kadane's algorithm.

--- amazon-practice-1.cpp
+++ amazon-practice-1.cpp
@@ -1,5 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class Solution{
+  public:
+
 /* 1. Minimum number of Jumps */
 /* Given an array of N integers arr[] where each element represents 
 the max length of the jump that can be made forward from that element. 
@@ -9,9 +13,6 @@ move through that element.
 Note: Return -1 if you can't reach the end of the array.
 https://practice.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1?page=1&difficulty[]=1&company[]=Amazon&curated[]=1&sortBy=submissions
 */
-
-class Solution{
-  public:
     int minJumps(int arr[], int n){
         // Your code here
         if(n==1)
@@ -38,7 +39,36 @@ class Solution{
             return jump;
         return -1;
     }
+
+
+/*    Kadane's Algorithm
+Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.
+https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1?page=1&difficulty[]=1&company[]=Amazon&curated[]=1&sortBy=submissions
+*/
+    long long max(long long a, long long b){
+        if(a > b)
+            return a;
+        return b;
+    }
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        long long res=INT_MIN, sum=0;
+        
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+            res = max(res, sum);
+            sum = max(sum, 0);
+        }
+        return res;
+        
+    }
+
 };
+
 
 
 int main(){
