0735ba1f197310aabcadc7ae7fa59c051f57f31a
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-09-14 08:59:28 +0530

    correction - number of jumps.

--- amazon-practice-1.cpp
+++ amazon-practice-1.cpp
@@ -12,34 +12,72 @@ Find the minimum number of jumps to reach the end of the array
 move through that element.
 Note: Return -1 if you can't reach the end of the array.
 https://practice.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1?page=1&difficulty[]=1&company[]=Amazon&curated[]=1&sortBy=submissions
+-> try of the below example.
+7
+1 4 3 0 8 9 0
+7
+1 4 3 0 8 9 0
 */
+  int max(int x, int y){
+    return (x > y) ? x : y;
+  }
+  
+  //public:
     int minJumps(int arr[], int n){
         // Your code here
-        if(n==1)
+        // The number of jumps needed to
+        // reach the starting index is 0
+        if (n <= 1)
             return 0;
-        if(arr[0]==0)
-            return -1;
-        
-        int hault=0, jump =0, maxi=0;
-        for(int i=0; i<n; i++){
-            if(arr[i]==0)
-                return -1;
-            maxi = max(maxi,i+arr[i]);
-            if(maxi>=n-1){
-                jump++;
-                return jump;
-            }
-            if(i==hault){
-                hault=maxi;
-                jump++;
-            }
-        }    
-        
-        if(hault>=n-1)
-            return jump;
-        return -1;
-    }
 
+        // Return -1 if not possible to jump
+        if (arr[0] == 0)
+            return -1;
+
+        // initialization
+        // stores all time the maximal
+        // reachable index in the array.
+        int maxReach = arr[0];
+
+        // stores the number of steps
+        // we can still take
+        int step = arr[0];
+
+        // stores the number of jumps
+        // necessary to reach that maximal
+        // reachable position.
+        int jump = 1;
+
+        // Start traversing array
+        int i = 1;
+        for (i = 1; i < n; i++) {
+            // Check if we have reached the end of the array
+            if (i == n - 1)
+                return jump;
+
+            // updating maxReach
+            maxReach = max(maxReach, i + arr[i]);
+
+            // we use a step to get to the current index
+            step--;
+
+            // If no further steps left
+            if (step == 0) {
+                // we must have used a jump
+                jump++;
+
+                // Check if the current index/position or lesser index
+                // is the maximum reach point from the previous indexes
+                if (i >= maxReach)
+                    return -1;
+
+                // re-initialize the steps to the amount
+                // of steps to reach maxReach from position i.
+                step = maxReach - i;
+            }
+        }
+    return -1;
+    }
 
 /*    Kadane's Algorithm
 Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.
