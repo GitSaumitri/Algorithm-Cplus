a3c5708bfb45ccc8431c6959d4ddbf98d5fb29d5
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-09-20 00:22:58 +0530

    kth element.

--- striver-placement-series.cpp
+++ striver-placement-series.cpp
@@ -1746,6 +1746,36 @@ class placement{
         }
         return 0.0;
     }
+    /* 65. Kth element of two sorted array - GFG - binary search
+        - brute force - two pointers pointing to two arrays - O(K) - space - O(1)
+        - divide n concure - O(logN+logM) - O(logN)
+        - binary search - O(log(min(M,N))) - O(1)  
+    */
+    int kthElement(int arr1[], int arr2[], int n, int m, int k){
+        if(n > m)
+            return kthElement(arr2, arr1, m, n, k);
+        
+        int low = max(0,k-m), high = min(k,n);
+
+        while(low <= high){
+            int cut1 = (low+high) >> 1;
+            int cut2 = k - cut1;
+
+            int l1 = cut1 == 0 ? INT_MIN : arr1[cut1 - 1];
+            int l2 = cut2 == 0 ? INT_MIN : arr2[cut2 - 1];
+            int r1 = cut1 == n ? INT_MAX : arr1[cut1];
+            int r2 = cut2 == m ? INT_MAX : arr2[cut2];
+
+            if(l1 <= r2 && l2 <= r1){
+                return max(l1, l2);
+            } else if(l1 > r2){
+                high = cut1 - 1;
+            } else {
+                low = cut1 + 1;
+            }
+        }
+        return 1;
+    }
 
     //helper functions.
     /* create a linked list */
