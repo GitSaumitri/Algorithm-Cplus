0a71489f2cfe404c4459ef8a38ede6d455710c2b
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-10-01 13:39:23 +0530

    string segmentation.

--- educative-amazon-45.cpp
+++ educative-amazon-45.cpp
@@ -147,6 +147,133 @@ LinkedListNode1* deep_copy_arbitrary_pointer(
 //The memory complexity of this solution is constant, O(1).
 
 //5. Level Order Traversal of Binary Tree
+// Using two queues
+void level_order_traversal_1(BinaryTreeNode* root) {
+  if (root == nullptr) {
+    return;
+  }
+
+  queue<BinaryTreeNode*> queues[2];
+  queue<BinaryTreeNode*>* current_queue = &queues[0];
+  queue<BinaryTreeNode*>* next_queue = &queues[1];
+
+  current_queue->push(root);
+  int level_number = 0;
+
+  while (!current_queue->empty()) {
+    BinaryTreeNode* temp = current_queue->front();
+    current_queue->pop();
+    cout << temp->data << ",";
+
+    if (temp->left != nullptr) {
+      next_queue->push(temp->left);
+    }
+
+    if (temp->right != nullptr) {
+      next_queue->push(temp->right);
+    }
+
+    if (current_queue->empty()) {
+      cout << endl;
+      ++level_number;
+      current_queue = &queues[level_number % 2];
+      next_queue = &queues[(level_number + 1) % 2];
+    }
+  }
+  cout << endl;
+}
+//The runtime complexity of this solution is linear, O(n).
+//The memory complexity of this solution is linear, O(n).
+
+// using single queue
+string level_order_traversal(BinaryTreeNode* root) {
+  string result = "";
+  //TODO: Write - Your - Code
+  queue<BinaryTreeNode*> q;
+  q.push(root);
+  while(!q.empty()){
+    int len = q.size();
+    for(int i=0; i<len; i++){
+      BinaryTreeNode *node = q.front();
+      q.pop();
+      result += to_string(node->data)+",";
+      if(node->left)
+        q.push(node->left);
+      if(node->right)
+        q.push(node->right);
+    }
+  } 
+  return result;
+}
+//The runtime complexity of this solution is linear, O(n).
+//The memory complexity of this solution is linear, O(n).
+
+//6. Determine if a binary tree is a binary search tree
+bool is_bst_rec(
+    BinaryTreeNode* root,
+    int min_value,
+    int max_value) {
+
+  if (root == nullptr) {
+    return true;
+  }
+
+  if (root->data < min_value || 
+      root->data > max_value) {
+    return false;
+  }
+
+  return 
+    is_bst_rec(root->left, min_value, root->data) &&
+      is_bst_rec(root->right, root->data, max_value);
+}
+
+bool is_bst(BinaryTreeNode* root) {
+  return 
+    is_bst_rec(
+      root,
+      numeric_limits<int>::min(), 
+      numeric_limits<int>::max());
+}
+//The runtime complexity of this solution is linear, O(n).
+//The memory complexity of this solution is linear, O(n).
+
+bool is_bst_tree(BinaryTreeNode* root, BinaryTreeNode* low, BinaryTreeNode* high){
+    if(root==nullptr){
+        return true;
+    }
+    if(low!=nullptr && (root->data < low->data))
+        return false;
+        
+    if(high!=nullptr && (root->data < high->data))
+        return false;
+    
+    return (!is_bst_tree(root->left, nullptr, root) &&
+        (!is_bst_tree(root->right, root, nullptr)));
+}
+
+bool is_bst(BinaryTreeNode* root) {
+    return is_bst_tree(root, nullptr, nullptr);
+}
+
+/*
+7. String segmentation
+You are given a dictionary of words and a large input string. 
+You have to find out whether the input string can be completely segmented into the words of a given dictionary. 
+*/
+bool can_segment_string(string s, unordered_set <string> & dictonary) {
+  for (int i = 1; i <= s.length(); ++i) {
+    string first = s.substr(0, i);
+    if (dictonary.find(first) != dictonary.end()) {
+      string second = s.substr(i);
+      if (second.empty() || dictonary.find(second) != dictonary.end() || can_segment_string(second, dictonary)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 
 
 int main(){
