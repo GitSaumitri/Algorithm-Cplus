f6b69dac5f9c4dca1fe90fa328716bc08f4ad430
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-10-02 07:11:14 +0530

    max subarray sum.

--- rahul-blind-75.cpp
+++ rahul-blind-75.cpp
@@ -1,97 +1,211 @@
 /* Blind LC-75 
-- Two Sum - https://leetcode.com/problems/two-sum/
-- Best Time to Buy and Sell Stock - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-- Contains Duplicate - https://leetcode.com/problems/contains-duplicate/
-- Product of Array Except Self - https://leetcode.com/problems/product-of-array-except-self/
-- Maximum Subarray - https://leetcode.com/problems/maximum-subarray/
-- Maximum Product Subarray - https://leetcode.com/problems/maximum-product-subarray/
-- Find Minimum in Rotated Sorted Array - https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
-- Search in Rotated Sorted Array - https://leetcode.com/problems/search-in-rotated-sorted-array/
-- 3Sum - https://leetcode.com/problems/3sum/
-- Container With Most Water - https://leetcode.com/problems/container-with-most-water/
+1- Two Sum - https://leetcode.com/problems/two-sum/
+2- Best Time to Buy and Sell Stock - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+3- Contains Duplicate - https://leetcode.com/problems/contains-duplicate/
+4- Product of Array Except Self - https://leetcode.com/problems/product-of-array-except-self/
+5- Maximum Subarray - https://leetcode.com/problems/maximum-subarray/
+6- Maximum Product Subarray - https://leetcode.com/problems/maximum-product-subarray/
+7- Find Minimum in Rotated Sorted Array - https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+8- Search in Rotated Sorted Array - https://leetcode.com/problems/search-in-rotated-sorted-array/
+9- 3Sum - https://leetcode.com/problems/3sum/
+10- Container With Most Water - https://leetcode.com/problems/container-with-most-water/
 Binary
-- Sum of Two Integers - https://leetcode.com/problems/sum-of-two-integers/
-- Number of 1 Bits - https://leetcode.com/problems/number-of-1-bits/
-- Counting Bits - https://leetcode.com/problems/counting-bits/
-- Missing Number - https://leetcode.com/problems/missing-number/
-- Reverse Bits - https://leetcode.com/problems/reverse-bits/
+11- Sum of Two Integers - https://leetcode.com/problems/sum-of-two-integers/
+12- Number of 1 Bits - https://leetcode.com/problems/number-of-1-bits/
+13- Counting Bits - https://leetcode.com/problems/counting-bits/
+14- Missing Number - https://leetcode.com/problems/missing-number/
+15- Reverse Bits - https://leetcode.com/problems/reverse-bits/
 Dynamic Programming
-- Climbing Stairs - https://leetcode.com/problems/climbing-stairs/
-- Coin Change - https://leetcode.com/problems/coin-change/
-- Longest Increasing Subsequence - https://leetcode.com/problems/longest-increasing-subsequence/
-- Longest Common Subsequence -
-- Word Break Problem - https://leetcode.com/problems/word-break/
-- Combination Sum - https://leetcode.com/problems/combination-sum-iv/
-- House Robber - https://leetcode.com/problems/house-robber/
-- House Robber II - https://leetcode.com/problems/house-robber-ii/
-- Decode Ways - https://leetcode.com/problems/decode-ways/
-- Unique Paths - https://leetcode.com/problems/unique-paths/
-- Jump Game - https://leetcode.com/problems/jump-game/
+16- Climbing Stairs - https://leetcode.com/problems/climbing-stairs/
+17- Coin Change - https://leetcode.com/problems/coin-change/
+18- Longest Increasing Subsequence - https://leetcode.com/problems/longest-increasing-subsequence/
+19- Longest Common Subsequence -
+20- Word Break Problem - https://leetcode.com/problems/word-break/
+21- Combination Sum - https://leetcode.com/problems/combination-sum-iv/
+22- House Robber - https://leetcode.com/problems/house-robber/
+23- House Robber II - https://leetcode.com/problems/house-robber-ii/
+24- Decode Ways - https://leetcode.com/problems/decode-ways/
+25- Unique Paths - https://leetcode.com/problems/unique-paths/
+26- Jump Game - https://leetcode.com/problems/jump-game/
 Graph
-- Clone Graph - https://leetcode.com/problems/clone-graph/
-- Course Schedule - https://leetcode.com/problems/course-schedule/
-- Pacific Atlantic Water Flow - https://leetcode.com/problems/pacific-atlantic-water-flow/
-- Number of Islands - https://leetcode.com/problems/number-of-islands/
-- Longest Consecutive Sequence - https://leetcode.com/problems/longest-consecutive-sequence/
-- Alien Dictionary (Leetcode Premium) - https://leetcode.com/problems/alien-dictionary/
-- Graph Valid Tree (Leetcode Premium) - https://leetcode.com/problems/graph-valid-tree/
-"- Number of Connected Components in an Undirected Graph (Leetcode Premium) - 
+27- Clone Graph - https://leetcode.com/problems/clone-graph/
+28- Course Schedule - https://leetcode.com/problems/course-schedule/
+29- Pacific Atlantic Water Flow - https://leetcode.com/problems/pacific-atlantic-water-flow/
+30- Number of Islands - https://leetcode.com/problems/number-of-islands/
+31- Longest Consecutive Sequence - https://leetcode.com/problems/longest-consecutive-sequence/
+32- Alien Dictionary (Leetcode Premium) - https://leetcode.com/problems/alien-dictionary/
+33- Graph Valid Tree (Leetcode Premium) - https://leetcode.com/problems/graph-valid-tree/
+34- Number of Connected Components in an Undirected Graph (Leetcode Premium) - 
 https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/"
 Interval
-- Insert Interval - https://leetcode.com/problems/insert-interval/
-- Merge Intervals - https://leetcode.com/problems/merge-intervals/
-- Non-overlapping Intervals - https://leetcode.com/problems/non-overlapping-intervals/
-- Meeting Rooms (Leetcode Premium) - https://leetcode.com/problems/meeting-rooms/
-- Meeting Rooms II (Leetcode Premium) - https://leetcode.com/problems/meeting-rooms-ii/
+35- Insert Interval - https://leetcode.com/problems/insert-interval/
+36- Merge Intervals - https://leetcode.com/problems/merge-intervals/
+37- Non-overlapping Intervals - https://leetcode.com/problems/non-overlapping-intervals/
+38- Meeting Rooms (Leetcode Premium) - https://leetcode.com/problems/meeting-rooms/
+39- Meeting Rooms II (Leetcode Premium) - https://leetcode.com/problems/meeting-rooms-ii/
 Linked List
-- Reverse a Linked List - https://leetcode.com/problems/reverse-linked-list/
-- Detect Cycle in a Linked List - https://leetcode.com/problems/linked-list-cycle/
-- Merge Two Sorted Lists - https://leetcode.com/problems/merge-two-sorted-lists/
-- Merge K Sorted Lists - https://leetcode.com/problems/merge-k-sorted-lists/
-- Remove Nth Node From End Of List - https://leetcode.com/problems/remove-nth-node-from-end-of-list/
-- Reorder List - https://leetcode.com/problems/reorder-list/
+40- Reverse a Linked List - https://leetcode.com/problems/reverse-linked-list/
+41- Detect Cycle in a Linked List - https://leetcode.com/problems/linked-list-cycle/
+42- Merge Two Sorted Lists - https://leetcode.com/problems/merge-two-sorted-lists/
+43- Merge K Sorted Lists - https://leetcode.com/problems/merge-k-sorted-lists/
+44- Remove Nth Node From End Of List - https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+45- Reorder List - https://leetcode.com/problems/reorder-list/
 Matrix
-- Set Matrix Zeroes - https://leetcode.com/problems/set-matrix-zeroes/
-- Spiral Matrix - https://leetcode.com/problems/spiral-matrix/
-- Rotate Image - https://leetcode.com/problems/rotate-image/
-- Word Search - https://leetcode.com/problems/word-search/
+46- Set Matrix Zeroes - https://leetcode.com/problems/set-matrix-zeroes/
+47- Spiral Matrix - https://leetcode.com/problems/spiral-matrix/
+48- Rotate Image - https://leetcode.com/problems/rotate-image/
+49- Word Search - https://leetcode.com/problems/word-search/
 String
-- Longest Substring Without Repeating Characters - https://leetcode.com/problems/longest-substring-without-repeating-characters/
-- Longest Repeating Character Replacement - https://leetcode.com/problems/longest-repeating-character-replacement/
-- Minimum Window Substring - https://leetcode.com/problems/minimum-window-substring/
-- Valid Anagram - https://leetcode.com/problems/valid-anagram/
-- Group Anagrams - https://leetcode.com/problems/group-anagrams/
-- Valid Parentheses - https://leetcode.com/problems/valid-parentheses/
-- Valid Palindrome - https://leetcode.com/problems/valid-palindrome/
-- Longest Palindromic Substring - https://leetcode.com/problems/longest-palindromic-substring/
-- Palindromic Substrings - https://leetcode.com/problems/palindromic-substrings/
-- Encode and Decode Strings (Leetcode Premium) - https://leetcode.com/problems/encode-and-decode-strings/
+50- Longest Substring Without Repeating Characters - https://leetcode.com/problems/longest-substring-without-repeating-characters/
+51- Longest Repeating Character Replacement - https://leetcode.com/problems/longest-repeating-character-replacement/
+52- Minimum Window Substring - https://leetcode.com/problems/minimum-window-substring/
+53- Valid Anagram - https://leetcode.com/problems/valid-anagram/
+54- Group Anagrams - https://leetcode.com/problems/group-anagrams/
+55- Valid Parentheses - https://leetcode.com/problems/valid-parentheses/
+56- Valid Palindrome - https://leetcode.com/problems/valid-palindrome/
+57- Longest Palindromic Substring - https://leetcode.com/problems/longest-palindromic-substring/
+58- Palindromic Substrings - https://leetcode.com/problems/palindromic-substrings/
+59- Encode and Decode Strings (Leetcode Premium) - https://leetcode.com/problems/encode-and-decode-strings/
 Tree
-- Maximum Depth of Binary Tree - https://leetcode.com/problems/maximum-depth-of-binary-tree/
-- Same Tree - https://leetcode.com/problems/same-tree/
-- Invert/Flip Binary Tree - https://leetcode.com/problems/invert-binary-tree/
-- Binary Tree Maximum Path Sum - https://leetcode.com/problems/binary-tree-maximum-path-sum/
-- Binary Tree Level Order Traversal - https://leetcode.com/problems/binary-tree-level-order-traversal/
-- Serialize and Deserialize Binary Tree - https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
-- Subtree of Another Tree - https://leetcode.com/problems/subtree-of-another-tree/
-"- Construct Binary Tree from Preorder and Inorder Traversal - 
+60- Maximum Depth of Binary Tree - https://leetcode.com/problems/maximum-depth-of-binary-tree/
+61- Same Tree - https://leetcode.com/problems/same-tree/
+62- Invert/Flip Binary Tree - https://leetcode.com/problems/invert-binary-tree/
+63- Binary Tree Maximum Path Sum - https://leetcode.com/problems/binary-tree-maximum-path-sum/
+64- Binary Tree Level Order Traversal - https://leetcode.com/problems/binary-tree-level-order-traversal/
+65- Serialize and Deserialize Binary Tree - https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+66- Subtree of Another Tree - https://leetcode.com/problems/subtree-of-another-tree/
+67- Construct Binary Tree from Preorder and Inorder Traversal - 
 https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/"
-- Validate Binary Search Tree - https://leetcode.com/problems/validate-binary-search-tree/
-- Kth Smallest Element in a BST - https://leetcode.com/problems/kth-smallest-element-in-a-bst/
-- Lowest Common Ancestor of BST - https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
-- Implement Trie (Prefix Tree) - https://leetcode.com/problems/implement-trie-prefix-tree/
-- Add and Search Word - https://leetcode.com/problems/add-and-search-word-data-structure-design/
-- Word Search II - https://leetcode.com/problems/word-search-ii/
+68- Validate Binary Search Tree - https://leetcode.com/problems/validate-binary-search-tree/
+69- Kth Smallest Element in a BST - https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+70- Lowest Common Ancestor of BST - https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+71- Implement Trie (Prefix Tree) - https://leetcode.com/problems/implement-trie-prefix-tree/
+72- Add and Search Word - https://leetcode.com/problems/add-and-search-word-data-structure-design/
+73- Word Search II - https://leetcode.com/problems/word-search-ii/
 Heap
-- Merge K Sorted Lists - https://leetcode.com/problems/merge-k-sorted-lists/
-- Top K Frequent Elements - https://leetcode.com/problems/top-k-frequent-elements/
-- Find Median from Data Stream - https://leetcode.com/problems/find-median-from-data-stream/
+74- Merge K Sorted Lists - https://leetcode.com/problems/merge-k-sorted-lists/
+75- Top K Frequent Elements - https://leetcode.com/problems/top-k-frequent-elements/
+76- Find Median from Data Stream - https://leetcode.com/problems/find-median-from-data-stream/
 */
 
 using namespace std;
 #include<iostream>
 #include<bits/stdc++.h>
 
+
+class Solution {
+public:
+    //1- Two Sum - https://leetcode.com/problems/two-sum/  
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unordered_map<int, int> m;
+        for(int i=0; i<nums.size(); i++){
+            if(m.find(target-nums[i])!=m.end()){
+                res.emplace_back(i);
+                res.emplace_back(m[target-nums[i]]);
+                return res;
+            }
+            m[nums[i]]=i;
+        }
+        return res;
+    }
+
+    //2- Best Time to Buy and Sell Stock - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    int maxProfit(vector<int>& prices) {
+        int lsf = INT_MAX; // least so far
+        int op = 0; // overall profit
+        int pist = 0; // profit if sold today
+        
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < lsf){ // if we found new buy value which is more smaller then previous one
+                lsf = prices[i]; // update our least so far
+            }
+            pist = prices[i] - lsf; // calculating profit if sold today by, Buy - sell
+            if(op < pist){ // if pist is more then our previous overall profit
+                op = pist; // update overall profit
+            }
+        }
+        return op; // return op      
+    }
+
+    int maxProfit1(vector<int>& prices) {
+        int maax=0;
+        int miin=prices[0];
+        for(int i=0 ; i<prices.size() ; i++){
+            miin=min(prices[i],miin);
+            maax=max(prices[i]-miin,maax);
+        }
+        return maax;     
+    }
+    //3- Contains Duplicate - https://leetcode.com/problems/contains-duplicate/
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map <int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.count(nums[i]) > 0) {
+                return true;
+            }
+            mp[nums[i]] += 1;
+        }
+        return false;
+    }
+
+    //4- Product of Array Except Self - https://leetcode.com/problems/product-of-array-except-self/
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int len = nums.size();
+        vector<int> left(len,1);
+        vector<int> right(len,1);
+        vector<int> res(len,1);
+        
+        left[0] = nums[0];
+        for(int i=1; i<len; i++){
+            left[i] = left[i-1]*nums[i];
+        }
+        
+        right[len-1]=nums[len-1];
+        for(int j=len-2; j>=0; j--){
+            right[j] = right[j+1]*nums[j];
+        }
+
+        for(int i=0; i<len; i++){
+            res[i] = (i-1>=0?left[i-1]:1) * (i+1<len?right[i+1]:1);
+        }
+        return res;
+    }
+
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int left = 1, right = 1, n = nums.size();
+        vector<int> res(n, 1);
+        for(int i=0; i<n; i++) {
+            res[i] *= left;
+            res[n-i-1] *= right;
+            left *= nums[i];
+            right *= nums[n-i-1];
+        }
+        return res;
+    }
+    
+    //5- Maximum Subarray - https://leetcode.com/problems/maximum-subarray/
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int maxi= nums[0];
+        for(int i =0 ; i<nums.size();i++){
+            sum += nums[i];
+            maxi = max(maxi,sum);
+            if(sum < 0)
+                sum = 0;
+        }
+        return maxi;
+    }
+
+    
+};
+
+
+//6- Maximum Product Subarray - https://leetcode.com/problems/maximum-product-subarray/
+//7- Find Minimum in Rotated Sorted Array - https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+//8- Search in Rotated Sorted Array - https://leetcode.com/problems/search-in-rotated-sorted-array/
+//9- 3Sum - https://leetcode.com/problems/3sum/
+//10- Container With Most Water - https://leetcode.com/problems/container-with-most-water/
 
 int main(){
     return 0;
