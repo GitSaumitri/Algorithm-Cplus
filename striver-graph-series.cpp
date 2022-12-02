f2e49060dd8d37ff53b4274e4cbf56500306c8df
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-12-03 00:41:11 +0530

    Update striver-graph-series.cpp

--- striver-graph-series.cpp
+++ striver-graph-series.cpp
@@ -563,8 +563,52 @@ int numberOfEnclaves(vector<vector<int>> &grid){
 }
 
 /* 16. Number of distinct Islands
+   - Given a boolean 2D matrix grid of size n * m. 
+   You have to find the number of distinct islands where a group of connected 1s 
+   (horizontally or vertically) forms an island. Two islands are considered to be 
+   distinct if and only if one island is not equal to another (not rotated or reflected)
+   space: O(n*m)
+   time: O(n*m)
+*/
 
+void dfs(int row, int col, vector<vector<int>>& vis,
+     vector<vector<int>>& grid, vector<vector<int>>& vec, 
+     int row0, int col0){
+    int n = grid.size();
+    int m = grid[0].size();
+    int delrow[] = {-1, 0, +1, 0};
+    int delcol[] = {0, -1, 0, +1};
 
+    vis[row][col] = 1;
+    vec.push_back({row-row0, col-col0});
+    for(int i=0; i<4; i++){
+        int nrow = row + delrow[i];
+        int ncol = col + delcol[i];
+        if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
+        && !vis[nrow][ncol] && grid[nrow][ncol]==1){
+            dfs(nrow, ncol, vis, grid, vec, row0, col0);
+        }
+    }        
+} 
+
+int countDistinctIslands(vector<vector<int>>& grid){
+    int n = grid.size();
+    int m = grid[0].size();
+    vector<vector<int>> vis(n, vector<int>(n,0));
+    set<vector<pair<int,int>>> st;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(!vis[i][j] && grid[i][j] == 1){
+                vector<pair<int,int>> vec;
+                dfs(i, j, grid, vis, vec);
+                st.insert(vec);
+            }
+        }
+    }
+    return st.size();
+}
+
+/* 17. Bipartite Graph
 */
 
 int main(){
