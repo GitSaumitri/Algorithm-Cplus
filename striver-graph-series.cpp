4bb8b79ab366ca35ce43be47841707b711733107
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-12-02 13:51:57 +0530

    Update striver-graph-series.cpp

--- striver-graph-series.cpp
+++ striver-graph-series.cpp
@@ -405,7 +405,48 @@ bool isCycle1(int V, vector<int> adj[]){
     - nearest distance so think about BFS
     - with 1 step - reach all 0 at a distance 1, with step 2, each all 0 at distance 2
     - multi source BFS, put all source at queue and stat BFS
+    time : O(n*m)
+    space : O(n*m)
 */
+vector<vector<int> nearest(vector<vector<int>> grid) {
+    int n = grid.size()
+    int m = grid[0].size();
+    vector<vector<int>> vis(n, vector<int>(m,0));
+    vector<vector<int>> dist(n, vector<int>(m,0));
+    queue<pair<pair<int,int>, int>> q;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            if(grid[i][j] == 1) {
+                q.push({{i,j},0});
+                vis[i][j] = 1;
+            } else {
+                vis[i][j] = 0;
+            }
+        }
+    }
+    
+    int delrow[] = {-1, 0, +1, 0};
+    int delcol[] = {0, +1, 0, -1};
+
+    while(!q.emptry()){
+        int row = q.front().first.first;
+        int col = q.front().first.second;
+        int steps = q.front().second;
+        q.pop();
+        dist[row][col] = steps;
+
+        for(int i=0; i<4; i++){
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+            if(nrow >= 0 && nrow < n && ncol >=0 && ncol < m
+                && vis[nrow][ncol] == 0) {
+                vis[nrow][ncol] = 1;
+                q.push({{nrow,ncol}, steps+1});
+            }
+        }
+    }
+    return dist;
+}
 
 
 int main(){
