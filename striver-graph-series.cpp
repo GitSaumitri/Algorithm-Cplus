f0b1cebc452edecc0c1a7507875404bf31e637d8
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-12-10 13:31:59 +0530

    Update striver-graph-series.cpp

--- striver-graph-series.cpp
+++ striver-graph-series.cpp
@@ -903,6 +903,48 @@ bool isCyclic(int V, vector<int> adj[]){
 }
 
 /* 24. Course schedule - I and II
+   - something before something, thing about topo sort
+*/
+bool isPossible(int V, vector<pair<int,int>> &prereq){
+    vector<int> adj[V];
+    for(auto it: prereq){
+        adj[it.first].push_back(it.second);
+    }
+
+    int indegree[V]={0};
+    for(int i=0; i<V; i++){
+        for(auto it: adj[i])
+            indegree[it]++;
+    }
+
+    queue<int> q;
+    for(int i=0; i<V; i++){
+        if(indegree[i]==0)
+            q.push(i);
+    }
+
+    vector<int> topo;
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        topo.push_back(node);
+
+        //node is in your topo sort
+        for(auto it: adj[node]){
+            indegree[it]--;
+            if(indegree[it] == 0)
+                q.push(it);
+        }
+    }
+
+    //if order is asked, return topo array.
+    if(topo.size() == V)
+        return true;
+    return false;
+}
+
+/* 25. Find eventual safe states - BFS - toposort
+
 */
 
 
