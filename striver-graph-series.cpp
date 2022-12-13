91a664dbae3c62c98c92136b5f77825607f0466b
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-12-13 23:50:07 +0530

    Update striver-graph-series.cpp

--- striver-graph-series.cpp
+++ striver-graph-series.cpp
@@ -1340,7 +1340,52 @@ Time - O(ElogV)
 
 /* 35. Print shortest path
    - where are you coming from with dijkstra
+   time: dijkstra + O(N) = O(ElogV) + O(N)
 */
+
+vector<int> shortestPath(int n, int m,
+   vector<vector<int>>&edges){
+    // 1 based index
+    vector<pair<int,int>> adj[n+1];
+    for(auto it: edges){
+        adj[it[0]].push_back({it[1],it[2]});
+        adj[it[1]].push_back({it[0],it[2]});
+    }
+
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    vector<int> dist(n+1, 1e9), parent(n+1);
+    for(int i=1; i<=n; i++)
+        parent[i] = i;
+    dist[1] = 0;
+    while(!pq.empty()){
+        auto it = pq.top();
+        int node = it.second;
+        int dis = it.first;
+        pq.pop();
+
+        for(auto it: adj[node]){
+            int adjNode = it.first;
+            int edW = it.second;
+            if(dis + edW < dist[adjNode]){
+                dist[adjNode] = dis + edW;
+                pq.push({dist+edW, adjNode});
+                parent[adjNode] = node;
+            }
+        }
+    }
+    if(dist[n] == 1e9) return {-1};
+    vector<int> path;
+    int node = n;
+    // O(N)
+    while(parent[node] != node){
+        path.push_back(node);
+        node = parent[node];
+    }
+    path.push_back(1);
+    reverse(path.begin(), path.end());
+    return path;
+}
+	
 	
 	
 int main(){
