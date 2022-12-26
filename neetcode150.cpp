85cfb72317f0676c74e8d2b70703c2c19476fa85
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-12-26 22:49:52 +0530

    Update neetcode150.cpp

--- neetcode150.cpp
+++ neetcode150.cpp
@@ -43,4 +43,22 @@
     }
 
 //4. https://leetcode.com/problems/group-anagrams/
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string,vector<string>>m;
+        vector<vector<string>> res;
+        
+        for(int i=0; i<strs.size(); i++){
+            string s = strs[i];
+            sort(s.begin(),s.end());
+            m[s].push_back(strs[i]);
+        }
+        for(auto it=m.begin(); it!=m.end(); it++){
+            res.push_back(it->second);
+        }
+        
+        return res;
+    }
+
+//5. https://leetcode.com/problems/top-k-frequent-elements/
+
 
