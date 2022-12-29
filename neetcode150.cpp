8098042663585800413507ab937d6a32deffd531
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2022-12-29 10:58:16 +0530

    Update neetcode150.cpp

--- neetcode150.cpp
+++ neetcode150.cpp
@@ -247,3 +247,53 @@ public:
     }
 };
 
+//https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+      stack<int> s;
+        for (auto a : tokens) {
+            if (a.size() == 1 && !isdigit(a[0])) {  // check if an operator
+                int num2 = s.top();
+                s.pop();
+                int num1 = s.top();
+                s.pop();
+                switch (a[0]) {  // note switch use char or integer
+                    case '+':
+                        s.push(num1 + num2);
+                        break;
+                    case '-':
+                        s.push(num1 - num2);
+                        break;
+                    case '*':
+                        s.push(num1 * num2);
+                        break;
+                    case '/':
+                        s.push(num1 / num2);
+                        break;
+                }
+            } else {  // if an number push to stack
+                s.push(atoi(a.c_str()));
+            }
+        }
+        return s.top();
+    }
+};
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        string t = tokens.back();
+        tokens.pop_back();
+        if(t != "+" && t != "-" && t != "*" && t != "/") return stoi(t);
+        else{
+            int b = evalRPN(tokens);
+            int a = evalRPN(tokens);
+            if(t == "+") return a + b;
+            else if(t == "-") return a - b;
+            else if(t == "*") return a * b;
+            else return a / b;
+        }
+    }
+};
