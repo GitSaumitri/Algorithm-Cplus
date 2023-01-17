c725e5d16d7003d30f85544bd45350d89eb163ce
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2023-01-17 08:52:36 +0530

    Update neetcode150_binarySearch.cpp

--- neetcode150_binarySearch.cpp
+++ neetcode150_binarySearch.cpp
@@ -1,1 +1,335 @@
+/*
+
+
+
+*/
+
+
+
+/*
+    Given sorted int array, search for a target value
+    Ex. nums = [-1,0,3,5,9,12], target = 9 -> 4 (index)
+
+    Since array is sorted, perform binary search
+
+    Time: O(log n)
+    Space: O(1)
+*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        
+        return -1;
+    }
+};
+
+/*
+    Search for target value in matrix where every row & col is sorted
+
+    Perform 2 binary searches: 1 to find row, then another to find col
+
+    Time: O(log m + log n)
+    Space: O(1)
+*/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int lowRow = 0;
+        int highRow = matrix.size() - 1;
+        
+        while (lowRow < highRow) {
+            int mid = lowRow + (highRow - lowRow) / 2;
+            if (matrix[mid][0] == target) {
+                return true;
+            }
+            if (matrix[mid][0] < target && target < matrix[mid + 1][0]) {
+                lowRow = mid;
+                break;
+            }
+            if (matrix[mid][0] < target) {
+                lowRow = mid + 1;
+            } else {
+                highRow = mid - 1;
+            }
+        }
+        
+        int lowCol = 0;
+        int highCol = matrix[0].size() - 1;
+        
+        while (lowCol <= highCol) {
+            int mid = lowCol + (highCol - lowCol) / 2;
+            if (matrix[lowRow][mid] == target) {
+                return true;
+            }
+            if (matrix[lowRow][mid] < target) {
+                lowCol = mid + 1;
+            } else {
+                highCol = mid - 1;
+            }
+        }
+        
+        return false;
+    }
+};
+
+
+/*
+    Given array of banana piles, guards are gone for h hours
+    Return min int k such that can eat all banans within h
+    Ex. piles = [3,6,7,11] h = 8 -> 4 (1@3, 2@6, 2@7, 3@11)
+
+    Binary search, for each k count hours needed, store min
+
+    Time: O(n x log m) -> n = # of piles, m = max # in a pile
+    Space: O(1)
+*/
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        
+        int low = 1;
+        int high = 0;
+        for (int i = 0; i < n; i++) {
+            high = max(high, piles[i]);
+        }
+        
+        int result = high;
+        
+        while (low <= high) {
+            int k = low + (high - low) / 2;
+            long int hours = 0;
+            for (int i = 0; i < n; i++) {
+                hours += ceil((double) piles[i] / k);
+            }
+            if (hours <= h) {
+                result = min(result, k);
+                high = k - 1;
+            } else {
+                low = k + 1;
+            }
+        }
+        
+        return result;
+    }
+};
+
+
+/*
+ * @lc app=leetcode id=153 lang=cpp
+ *
+ * [153] Find Minimum in Rotated Sorted Array
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+        // Neetcode solution Ologn time O1 space binary search
+        int res = nums[0];
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l <= r)
+        {
+            if (nums[l] < nums[r])
+            {
+                res = min(res, nums[l]);
+                break;
+            }
+            int mid = l + (r - l) / 2;
+            res = min(res, nums[mid]);
+
+            if (nums[mid] >= nums[l]) // mid present in left sorted array
+            {
+                l = mid + 1; // try to move closer to right sorted array
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+
+        return res;
+    }
+};
+// @lc code=end
+
+
+/*
+    Given array after some possible rotation, find if target is in nums
+    Ex. nums = [4,5,6,7,0,1,2] target = 0 -> 4 (value 0 is at index 4)
+
+    Modified binary search, if low <= mid left sorted, else right sorted
+
+    Time: O(log n)
+    Space: O(1)
+*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
+
+
+/*
+    Design time-based key-value structure, multiple vals at diff times
+
+    Hash map, since timestamps are naturally in order, binary search
+
+    Time: O(log n)
+    Space: O(n)
+*/
+
+class TimeMap {
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        m[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        if (m.find(key) == m.end()) {
+            return "";
+        }
+        
+        int low = 0;
+        int high = m[key].size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (m[key][mid].first < timestamp) {
+                low = mid + 1;
+            } else if (m[key][mid].first > timestamp) {
+                high = mid - 1;
+            } else {
+                return m[key][mid].second;
+            }
+        }
+        
+        if (high >= 0) {
+            return m[key][high].second;
+        }
+        return "";
+    }
+private:
+    unordered_map<string, vector<pair<int, string>>> m;
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
+
+
+/*
+    Given 2 sorted arrays of size m & n, return the median of these arrays
+    Ex. nums1 = [1,3] nums2 = [2] -> 2, nums1 = [1,2] nums2 = [3,4] -> 2.5
+
+    Binary search, partition each array until partitions are correct, get median
+    [1,2,3,4,5]
+    |  a|b    |
+    [1,2,3,4,5,6,7,8]    -->    a <= d ? yes, c <= b ? no, so need to fix
+    |      c|d      |
+
+    Time: O(log min(m, n))
+    Space: O(1)
+*/
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        
+        int total = m + n;
+        
+        int low = 0;
+        int high = m;
+        
+        double result = 0.0;
+        
+        while (low <= high) {
+            // nums1
+            int i = low + (high - low) / 2;
+            // nums2
+            int j = (total + 1) / 2 - i;
+            
+            int left1 = (i > 0) ? nums1[i - 1] : INT_MIN;
+            int right1 = (i < m) ? nums1[i] : INT_MAX;
+            int left2 = (j > 0) ? nums2[j - 1] : INT_MIN;
+            int right2 = (j < n) ? nums2[j] : INT_MAX;
+            
+            // partition is correct
+            if (left1 <= right2 && left2 <= right1) {
+                // even
+                if (total % 2 == 0) {
+                    result = (max(left1, left2) + min(right1, right2)) / 2.0;
+                // odd
+                } else {
+                    result = max(left1, left2);
+                }
+                break;
+            } else if (left1 > right2) {
+                high = i - 1;
+            } else {
+                low = i + 1;
+            }
+        }
+        
+        return result;
+    }
+};
 
