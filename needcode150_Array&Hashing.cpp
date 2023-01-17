6e0dbdd31844f3f852e2d4c85a0bbe85fb6d07fa
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2023-01-17 08:09:33 +0530

    Update needcode150_Array&Hashing.cpp

--- needcode150_Array&Hashing.cpp
+++ needcode150_Array&Hashing.cpp
@@ -314,6 +314,28 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int rows[9][9] = {0};
+        int cols[9][9] = {0};
+        int blocks[3][3][9] = {0};
+    
+        for(int r=0;r<9;r++){
+            for(int c=0;c<9;c++){
+                if(board[r][c]=='.')
+                    continue;
+                int num = board[r][c]-'1';
+                if(cols[num][c]++ || rows[r][num]++ || blocks[r/3][c/3][num]++)
+                    return false;
+            }
+        }
+    
+        return true;    
+    }
+};
+
 //8.  https://leetcode.com/problems/encode-and-decode-strings/
 /*
     Design algorithm to encode/decode: list of strings <-> string
