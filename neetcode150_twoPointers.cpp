762c14b0608fcc54063102833c80a5277ed67c14
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2023-01-17 08:33:20 +0530

    Update neetcode150_twoPointers.cpp

--- neetcode150_twoPointers.cpp
+++ neetcode150_twoPointers.cpp
@@ -1,1 +1,221 @@
+/* Two Pointers
+
+https://leetcode.com/problems/valid-palindrome/
+https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+https://leetcode.com/problems/3sum/
+https://leetcode.com/problems/container-with-most-water/
+https://leetcode.com/problems/trapping-rain-water/
+
+*/
+
+//https://leetcode.com/problems/valid-palindrome/
+/*
+    Given a string s, return true if it's a palindrome
+    Ex. s = "A man, a plan, a canal: Panama" -> true
+
+    2 pointers, outside in, skip non-letters & compare
+
+    Time: O(n)
+    Space: O(1)
+*/
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+        
+        while (i < j) {
+            while (!isalnum(s[i]) && i < j) {
+                i++;
+            }
+            while (!isalnum(s[j]) && i < j) {
+                j--;
+            }
+            if (tolower(s[i]) != tolower(s[j])) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+};
+
+
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+/*
+    Given a 1-indexed sorted int array & target:
+    Return indices (added by 1) of 2 nums that add to target
+
+    2 pointers, outside in, iterate i/j if sum is too low/high
+
+    Time: O(n)
+    Space: O(1)
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0;
+        int j = numbers.size() - 1;
+        
+        vector<int> result;
+        
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum < target) {
+                i++;
+            } else if (sum > target) {
+                j--;
+            } else {
+                result.push_back(i + 1);
+                result.push_back(j + 1);
+                break;
+            }
+        }
+        
+        return result;
+    }
+};
+
+//https://leetcode.com/problems/3sum/
+/*
+    Given int array, return all unique triplets that sum to 0
+    Ex. nums = [-1,0,1,2,-1,-4] -> [[-1,-1,2],[-1,0,1]]
+
+    Sort, for each i, have j & k go outside in, check for 0 sums
+
+    Time: O(n^2)
+    Space: O(n)
+*/
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        
+        int n = nums.size();
+        if (n < 3) {
+            return result;
+        }
+        
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < n - 2; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            
+            int j = i + 1;
+            int k = n - 1;
+            
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    result.push_back({nums[i], nums[j], nums[k]});
+                    
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    j++;
+                    
+                    while (j < k && nums[k - 1] == nums[k]) {
+                        k--;
+                    }
+                    k--;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
+
+
+//https://leetcode.com/problems/container-with-most-water/
+/*
+    Given array of heights, find max water container can store
+    Ex. height = [1,8,6,2,5,4,8,3,7] -> 49, (8 - 1) x min(8, 7)
+
+    2 pointers outside in, greedily iterate pointer w/ lower height
+
+    Time: O(n)
+    Space: O(1)
+*/
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i = 0;
+        int j = height.size() - 1;
+        
+        int curr = 0;
+        int result = 0;
+        
+        while (i < j) {
+            curr = (j - i) * min(height[i], height[j]);
+            result = max(result, curr);
+            
+            if (height[i] <= height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        
+        return result;
+    }
+};
+
+//https://leetcode.com/problems/trapping-rain-water/
+/*
+    Given elevation map array, compute trapped water
+    Ex. height = [0,1,0,2,1,0,1,3,2,1,2,1] -> 6
+
+    2 pointers, outside in, track max left/right
+    For lower max, curr only dependent on that one
+    Compute height of these, iterate lower one
+
+    Time: O(n)
+    Space: O(1)
+*/
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int i = 0;
+        int j = height.size() - 1;
+        
+        int maxLeft = height[i];
+        int maxRight = height[j];
+        
+        int result = 0;
+        
+        while (i < j) {
+            if (maxLeft <= maxRight) {
+                i++;
+                maxLeft = max(maxLeft, height[i]);
+                result += maxLeft - height[i];
+            } else {
+                j--;
+                maxRight = max(maxRight, height[j]);
+                result += maxRight - height[j];
+            }
+        }
+        
+        return result;
+    }
+};
+
+
 
