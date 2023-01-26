508839a404bd1dcfcec4210d0a76544e611f6961
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2023-01-26 09:12:51 +0530

    Update striver-graph-series.cpp

--- striver-graph-series.cpp
+++ striver-graph-series.cpp
@@ -1469,48 +1469,49 @@ int MinimumEffort(vector<vector<int>>&heights){
 /* 38. Cheapest Flights within K stops (IMP-IMP)
 https://practice.geeksforgeeks.org/problems/cheapest-flights-within-k-stops/1
 
+https://leetcode.com/problems/cheapest-flights-within-k-stops/
+
 - can't do based on weight - but based on number of stops
 - if based on weight - we will miss same stops with minimum distance
 - PQ not needed as the stop increment by 1
 - once reach dest - don't stop - check for optimise path
 */
 
-int cheapestFlight(int n, vector<vector<int>>& flights,
-                   int src, int dst, int K){
-
-    vector<pair<int,int>> adj[n];
-    for(auto it: flights){
-         adj[it[0]].push_back({it[1], it[2]});
-    }
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<pair<int,int>> adj[n];
+        for(auto it: flights){
+            adj[it[0]].push_back({it[1], it[2]});
+        }
     
-    queue<pair<int, pair<int,int>>> q;
-    // {stops, {node, dist}}
-    q.push({0, {src, 0});
-    vector<int> dist(n, 1e9);
-    dist[src] = 0;
-    // E log V is for dijkstra
-    // since no PQ so E = flights.size()
-    while(!q.empty()){
-        auto it = q.front();
-        q.pop();
-        int stops = it.first;
-        int node = it.second.first;
-        int cost = it.second.second;
+        queue<pair<int, pair<int,int>>> q;
+        // {stops, {node, dist}}
+        q.push({0, {src, 0}});
+        vector<int> dist(n, 1e9);
+        dist[src] = 0;
+        // E log V is for dijkstra
+        // since no PQ so E = flights.size()
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            int stops = it.first;
+            int node = it.second.first;
+            int cost = it.second.second;
 
-        if(stops > K) continue;
-        for(auto iter: adj[node]){
-            int adjNode = iter.first;
-            int edW = iter.second;
-            if(cost+edW < dist[adjNode] && stops <= K){
-                dist[adjNode] = cost + edW;
-                q.push({stops+1, {adjNode, cost+edW});
+            if(stops > k) continue;
+            for(auto iter: adj[node]){
+                int adjNode = iter.first;
+                int edW = iter.second;
+                if(cost+edW < dist[adjNode] && stops <= k){
+                    dist[adjNode] = cost + edW;
+                    q.push({stops+1, {adjNode, cost+edW}});
+                }
             }
         }
+        if(dist[dst] == 1e9) 
+            return -1;
+        return dist[dst];    
     }
-    if(dist[dst] == 1e9) return -1;
-    return dist[dst];
-}
-
+	
 /* 39. Minimum multiplication to reach end
 
 */
