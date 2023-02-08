41ec4cd5dcffc8eeded04af74b01a577466a4275
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2023-02-08 07:27:04 +0530

    Update amazon-practice-1.cpp

--- amazon-practice-1.cpp
+++ amazon-practice-1.cpp
@@ -29,6 +29,7 @@ class Solution{
 */
 
 /* 1. Minimum number of Jumps */
+    https://leetcode.com/problems/jump-game-ii/description/
 /* Given an array of N integers arr[] where each element represents 
 the max length of the jump that can be made forward from that element. 
 Find the minimum number of jumps to reach the end of the array 
