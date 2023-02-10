d7de68830ae8dd1638011b7b7fabc062ff77ae14
GitSaumitri <35699270+GitSaumitri@users.noreply.github.com>
2023-02-10 08:36:38 +0530

    Update amazon-practice-1.cpp

--- amazon-practice-1.cpp
+++ amazon-practice-1.cpp
@@ -28,8 +28,10 @@ class Solution{
 10. Alien Dictionary (hard)
 */
 
+    
 /* 1. Minimum number of Jumps */
-    https://leetcode.com/problems/jump-game-ii/description/
+https://leetcode.com/problems/jump-game
+https://leetcode.com/problems/jump-game-ii        
 /* Given an array of N integers arr[] where each element represents 
 the max length of the jump that can be made forward from that element. 
 Find the minimum number of jumps to reach the end of the array 
